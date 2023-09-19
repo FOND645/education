e5e9d604a512db7b1ecdf6c934ac33485b0de3a8
FOND645 <112339957+FOND645@users.noreply.github.com>
2023-09-19 16:32:55 +0300

    commit

--- sqLearn/JsonToDB.ts
+++ sqLearn/JsonToDB.ts
@@ -39,6 +39,29 @@ let defectsIDs: idsObject = {
 let actionsIDs: idsObject = {
     count: 0,
 };
+let organiztionsIDs = [
+    {
+        name: "144 БТРЗ",
+        city: "Екатеринбург"
+    },
+    {
+        name: "61 БТРЗ",
+        city: "Санкт-Петербург"
+    },
+    {
+        name: "81 БТРЗ",
+        city: "Армавир"
+    },
+]
+let contractsIDs: idsObject = {
+    count: 0
+}
+let repairDevicesIDs: idsObject = {
+    count: 0
+}
+let repairBlocksIDs: idsObject = {
+    count: 0
+}
 
 const contractsPath: string = path.join(__dirname, `dataBases/contracts.json`);
 const defectsPath: string = path.join(__dirname, `dataBases/defectsBase.json`);
@@ -375,4 +398,178 @@ db.serialize(() => {
             });
         });
     });
+
+    // Создаем БД организаций
+    db.run(`CREATE TABLE "organiztions" (
+        "id"	INTEGER NOT NULL UNIQUE,
+        "name"	TEXT NOT NULL,
+        "city"	TEXT NOT NULL,
+        PRIMARY KEY("id" AUTOINCREMENT)
+    );`, (err) => {
+        if (err) {
+            console.log(`Ошибка создания таблицы организаций`);
+        } else {
+            console.log(`Создана таблица организаций`)
+        }
+    })
+
+    // Создаем БД контрактов
+    db.run(`CREATE TABLE "contracts" (
+        "id"	INTEGER NOT NULL UNIQUE,
+        "number"	TEXT NOT NULL,
+        "date"	TEXT NOT NULL,
+        "organiztion_id"	INTEGER,
+        PRIMARY KEY("id" AUTOINCREMENT),
+        FOREIGN KEY (organiztion_id) REFERENCES organiztions(id)
+    );`, (err) => {
+        if (err) {
+            console.log(`Ошибка создания таблицы контрактов`);
+        } else {
+            console.log(`Создана таблица контрактов`)
+        }
+    })
+
+    // Создаем БД устройств в ремонте
+    db.run(`CREATE TABLE "repair_devices" (
+        "id"	INTEGER NOT NULL UNIQUE,
+        "contract_id"	INTEGER NOT NULL,
+        "device_id"	INTEGER NOT NULL,
+        "create_time"	INTEGER NOT NULL,
+        "change_time"	INTEGER NOT NULL,
+        "repair_number"	INTEGER NOT NULL,
+        "serial_number"	TEXT NOT NULL,
+        PRIMARY KEY("id" AUTOINCREMENT),
+        FOREIGN KEY ("contract_id") REFERENCES contracts(id),
+        FOREIGN KEY ("device_id") REFERENCES defects(id)
+    );`, (err) => {
+        if (err) {
+            console.log(`Ошибка создания таблицы устройств в ремонте`);
+        } else {
+            console.log(`Создана таблица устройств в ремонте`)
+        }
+    })
+
+    // Создаем БД блоков в ремонте
+    db.run(`CREATE TABLE "repair_blocks" (
+        "id"	INTEGER NOT NULL UNIQUE,
+        "block_id"	INTEGER NOT NULL,
+        "serial_number"	TEXT NOT NULL,
+        "count"	REAL NOT NULL,
+        PRIMARY KEY("id" AUTOINCREMENT),
+        FOREIGN KEY ("block_id") REFERENCES blocks(id)
+    );`, (err) => {
+        if (err) {
+            console.log(`Ошибка создания таблицы блоков в ремонте`);
+        } else {
+            console.log(`Создана таблица блоков в ремонте`)
+        }
+    })
+
+    // Создаем таблицу связи БЛОК в ремонте - НЕИСПРАВНОСТИ
+    db.run(
+        `CREATE TABLE "defects_in_repair" (
+            "repair_blocks_id" INTEGER NOT NULL,
+            "defect_id" INTEGER NOT NULL,
+            PRIMARY KEY (repair_blocks_id, defect_id),
+            FOREIGN KEY (repair_blocks_id) REFERENCES repair_blocks(id),
+            FOREIGN KEY (defect_id) REFERENCES defects(id)            
+    )`, (err) => {
+        if (err) {
+            console.log(`Ошибка создания таблицы связи БЛОК в ремонте - НЕИСПРАВНОСТИ`);
+        } else {
+            console.log(`Создана таблица связи БЛОК в ремонте - НЕИСПРАВНОСТИ`)
+        }
+    })
+
+    organiztionsIDs.forEach((Organization, id) => {
+        const { name, city } = Organization
+        db.run('INSERT INTO organiztions ("id", "name", "city") VALUES (?, ?, ?)', [id, name, city], (err) => {
+            if (err) {
+                console.log(`Ошибка добавления организации ${id} ${name} ${city}`, err);
+            } else {
+                console.log(`Добавлена организация ${id} ${name} ${city}`)
+            }
+        })
+    })
+
+    JSONdatabse.contracts.forEach(Contract => {
+        const { organizationName, contractDate, contractNumber, repairBase } = Contract
+        const ContractKey = Contract.key
+        let contractID: undefined | number;
+        if (contractsIDs.hasOwnProperty(ContractKey)) {
+            contractID = contractsIDs[ContractKey];
+        } else {
+            contractsIDs[ContractKey] = contractsIDs.count;
+            contractID = contractsIDs.count;
+            contractsIDs.count++;
+        }
+        let organiztionID = organiztionsIDs.findIndex(org => org.name === organizationName)
+
+        db.run(`INSERT INTO contracts ("id", "number", "date", "organiztion_id") VALUES (?, ?, ?, ?)`, [contractID, contractNumber, contractDate, organiztionID], (err) => {
+            if (err) {
+                console.log(`Ошибка добавления контракта ${contractID} ${contractNumber} ${organiztionID}`, err);
+            } else {
+                console.log(`Добавлен контракт ${contractID} ${contractNumber} ${organiztionID}`)
+            }
+        })
+        repairBase.forEach(RepairDevice => {
+            const {changeTime, createTime, deviceKey, repairNumber, serialNumber, subDevices} = RepairDevice
+            const RepairDeviceKey = RepairDevice.key
+            const deviceID = devicesIDs[deviceKey]
+            let repairDeviceID: undefined | number;
+            if (repairDevicesIDs.hasOwnProperty(RepairDeviceKey)) {
+                repairDeviceID = repairDevicesIDs[RepairDeviceKey];
+            } else {
+                repairDevicesIDs[RepairDeviceKey] = repairDevicesIDs.count;
+                repairDeviceID = repairDevicesIDs.count;
+                repairDevicesIDs.count++;
+            }
+
+            db.run(`INSERT INTO repair_devices ("id", "contract_id", "device_id", "create_time", "change_time", "repair_number", "serial_number") VALUES (?, ?, ?, ?, ?, ?, ?)`, 
+            [repairDeviceID, contractID, deviceID, createTime, changeTime, repairNumber, serialNumber],
+            (err) => {
+                if (err) {
+                    console.log(`Ошибка добавления устройства ремонта ${repairDeviceID}, ${contractID}, ${deviceID}, ${createTime}, ${changeTime}, ${repairNumber}, ${serialNumber}`, err);
+                } else {
+                    console.log(`Добавлено устройство ремонта ${repairNumber} ${repairDevicesIDs} ${deviceID} ${serialNumber}`)
+                }
+            })
+            subDevices.forEach(Block => {
+                const {subDeviceKey, serialNumber, count, defects} = Block
+                const blockKey = Block.key
+                const blockID = blocksIDs[subDeviceKey]
+                let repairBlockID: undefined | number;
+                if (repairBlocksIDs.hasOwnProperty(RepairDeviceKey)) {
+                    repairBlockID = repairBlocksIDs[subDeviceKey];
+                } else {
+                    repairBlocksIDs[subDeviceKey] = repairBlocksIDs.count;
+                    repairBlockID = repairBlocksIDs.count;
+                    repairBlocksIDs.count++;
+                }
+                db.run(`INSERT INTO repair_blocks ("id", "block_id", "serial_number", "count") VALUES (?, ?, ?, ?)`,
+                [repairBlockID, blockID, serialNumber, count],
+                (err) => {
+                    if (err) {
+                        console.log(`Ошибка добавления блока ремонта ${repairBlockID}, ${blockID}, ${serialNumber}, ${count}`, err);
+                    } else {
+                        console.log(`Добавлено блока ремонта ${blockID} ${serialNumber}`)
+                    }
+                })
+                defects.forEach(Defect => {
+                    const defectID = defectsIDs[Defect]
+                    db.run(`INSERT INTO defects_in_repair ("repair_blocks_id", "defect_id") VALUES (?, ?)`,
+                    [repairBlockID, defectID],
+                    (err) => {
+                        if (err) {
+                            console.log(`Ошибка добавления связи БЛОК ${repairBlockID} - НЕИСПРАВНОСТЬ ${defectID}`, err);
+                        } else {
+                            console.log(`Добавлена связь БЛОК ${repairBlockID} - НЕИСПРАВНОСТЬ ${defectID}`)
+                        }
+                    })
+                })
+            })
+        })
+    })
+
+    db.all(`SELECT `)
 });
